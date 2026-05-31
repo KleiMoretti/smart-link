f9104935609f0c60b0163d62aa00c13679dcc300
kleimoretti <johnleonardgualdahara@gmail.com>
2026-05-31 11:21:59 +0800

    a

---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA1Ao8-XBHXoqJfAPGWWOJ4k6LQJl0GZGc",
+    authDomain: "samrt-link.firebaseapp.com",
+    projectId: "samrt-link",
+    storageBucket: "samrt-link.appspot.com",
+    messagingSenderId: "756514405561",
+    appId: "1:756514405561:web:1f689559f88299b896ef68",
+    measurementId: "G-KQLR9PDCCQ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// optional lang analytics
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    return result.user;
+};
