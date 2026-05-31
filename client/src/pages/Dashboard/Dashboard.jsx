401cf82ea11427aa8e237aa299d2995f3fb93fa9
kleimoretti <johnleonardgualdahara@gmail.com>
2026-05-31 11:51:04 +0800

    a

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,27 @@
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function Dashboard() {
-    return
-    (
-        <>
-            <div>
-                <p>a</p>
-            </div>
-        </>
-    )
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (!user) {
+                navigate("/login");
+            } else {
+                console.log(user);
+            }
+        });
+
+        // 🔥 IMPORTANT: cleanup
+        return () => unsubscribe();
+    }, [navigate]);
+
+    return (
+        <div>
+            <p>a</p>
+        </div>
+    );
 }
