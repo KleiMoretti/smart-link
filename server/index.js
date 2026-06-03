2b3c88329259a6091b8c75a5bf30b376c3dc308a
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-03 22:51:00 +0800

    a

--- server/index.js
+++ server/index.js
@@ -11,12 +11,10 @@ admin.initializeApp({
 
 const app = express();
 
-// 1. TAMA: Mas pinalakas at mas detalyadong CORS configuration
 app.use(cors());
 
 app.use(express.json());
 
-// 2. Ang iyong mga routes
 app.use("/api", authRoutes);
 
 app.listen(5000)
