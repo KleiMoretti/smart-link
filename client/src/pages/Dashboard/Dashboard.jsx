34de08110972c566b9a392bbeda33d238a2b1da8
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-04 15:39:18 +0800

    a

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,85 +1,43 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { auth } from "../../firebase/firebase";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import Table from "../Dashboard/Table"
-import CreateLink from "../Dashboard/Create"
+
+import { useState } from "react"
+import "../../css/Dashboard.css"
+import Table from "./Table"
+
 
 export default function Dashboard() {
-    const navigate = useNavigate();
-    const [userDetails, setUserDetails] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    const [tab, setTab] = useState("")
-
-    const handleDashboard = () => setTab("Dashboard");
-    const handleSchedules = () => setTab("Schedule");
-    const handleSubscription = () => setTab("Subscription");
-    const handleSettings = () => setTab("Settings");
-    const handleCreateLink = () => setTab("CreateLink");
-
-
-
-    useEffect(() => {
-        setTab("Schedule");
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (!user) {
-                navigate("/login");
-                setLoading(true)
-
-            } else {
-                setUserDetails(user);
-                setLoading(false)
-
-            }
-        });
-
-        return () => unsubscribe();
-    }, [navigate]);
-
-    const handleSignOUt = async () => {
-        await signOut(auth)
-    }
-    if (loading) return <p>Loading...</p>
+    const [showLines, setShowLine] = useState(false)
 
     return (
-        <section className="p-10">
-            <nav className="flex justify-between items-center">
-                <div className="font-bold flex items-center">
-                    <p>FluxLink</p>
-                </div>
-                <div className="flex gap-5 flex items-center">
-                    <div>
-                        <p className={`m-0 cursor-pointer ${tab === "Dashboard" ? "font-bold" : ""}`} onClick={handleDashboard} >Dashboard</p>
-                    </div>
-                    <div>
-                        <p className={`m-0 cursor-pointer ${tab === "Schedule" ? "font-bold" : ""}`} onClick={handleSchedules}>Schedules</p>
-                    </div>
-                    <div>
-                        <p className={`m-0 cursor-pointer ${tab === "Subscription" ? "font-bold" : ""}`} onClick={handleSubscription}>Subscription</p>
-                    </div>
-                    <div>
-                        <p className={`m-0 cursor-pointer ${tab === "Settings" ? "font-bold" : ""}`} onClick={handleSettings}>Settings</p>
-                    </div>
-                </div>
+        <>
+            <nav className="border boder-gray-500">
+                <div className="flex justify-center h-20 items-center">
+                    <div className="flex lg:w-[80%] justify-between gap-20">
+                        <div className="flex items-center">
+                            <h2 className="tracking-tight font-ubuntu font-bold whitespace-nowrap md:text-[2vw]">FluxLink</h2>
+                        </div>
+                        <div className="flex gap-10 items-center font-medium ">
+                            <div>
+                                Schedules
+                            </div>
 
-                <div className={`bg-black text-white font-bold flex items-center p-2 rounded-md cursor-pointer  ${tab === "CreateLink" ? "hidden" : ""}`} onClick={handleCreateLink}>
-                    < p className="m-0" >Create Link</p>
+                            <div>
+                                Subscription
+                            </div>
+                            <div>
+                                Feedback
+                            </div>
+                            <div>
+                                Settings
+                            </div>
+                        </div>
+                        <div className="whitespace-nowrap flex bg-blue-500 hover:bg-blue-600 items-center px-3 py-2 text-white rounded-md font-bold cursor-pointer transition-colors duration-200">
+                            Create Link
+                        </div>
+                    </div>
                 </div>
             </nav>
 
-
-
-            {tab === "Dashboard" && "<Dashboard/>"}
-            {tab === "Schedule" && <Table profile={userDetails?.photoURL} name={userDetails?.displayName} email={userDetails?.email} />}
-            {tab === "Subscription" && "<Subscription />"}
-            {tab === "Settings" && "<Settings />"}
-            {tab === "CreateLink" && <CreateLink />}
-
-
-            <div className="text-white cursor-pointer bg-red-500 w-[100px] items-center flex justify-center p-2 rounded-md mt-4 hover:bg-red-400">
-                <p className="m-0" onClick={handleSignOUt}>Sign Out</p>
-            </div>
-        </section >
-    );
+            <Table />
+        </>
+    )
 }
