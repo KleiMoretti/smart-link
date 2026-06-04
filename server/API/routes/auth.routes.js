d9e5303ef39e702b354e88bb6fd26d2287fc1912
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-04 19:57:04 +0800

    a

--- server/API/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-// Palitan ng require ang import
-const express = require("express");
-const { SaveLink } = require("../controllers/link.controllers.js");
-
-const route = express.Router();
-
-route.post("/SaveLinks", SaveLink);
-
-module.exports = route;
