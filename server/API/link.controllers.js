fc3838fd335e3198fe5f1cd84a22c63fdf89734a
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-04 19:56:46 +0800

    a

---
+++ server/API/link.controllers.js
@@ -0,0 +1,80 @@
+import SupabaseConnect from "../db/supabaseClient.js";
+import admin from "firebase-admin";
+
+
+
+export const SaveLink = async (req, res) => {
+    try {
+
+        const firebaseUID = req.user.uid
+        const firebaseName = req.user.name || req.user.email || "Anonymous User";
+
+        const { Links } = req.body;
+
+        if (!Links || !Array.isArray(Links)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Data format."
+            });
+        }
+
+        const isValidTime = (time) => /^([01]\d|2[0-3]):([0-5]\d)$/.test(time);
+
+        const validDays = [
+            "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+        ];
+
+        const isValid = Links.every((item) => {
+            return (
+                item.title &&
+                item.title.length > 1 &&
+                item.title.length < 50 &&
+
+                item.link &&
+                item.link.startsWith("https://") &&
+
+                validDays.includes(item.day) &&
+
+                item.time &&
+                isValidTime(item.time)
+            );
+        });
+
+        if (!isValid) {
+            return res.status(400).json({
+                message: "Invalid Link Data",
+                success: false
+            });
+        }
+
+        const linksToInsert = Links.map(item => ({
+            title: item.title,
+            links: item.link,
+            day: item.day,
+            time: item.time,
+            uid: firebaseUID,
+            name: firebaseName
+        }));
+
+        const { data, error } = await SupabaseConnect
+            .from("Links")
+            .insert(linksToInsert)
+            .select();
+        //aaa
+        if (error) {
+            console.log("SUPABASE ERROaR:", error);
+            return res.status(500).json({ success: false, message: error.message });
+        }
+
+        return res.status(200).json({ success: true, data: data });
+
+    } catch (error) {
+        console.error("❌ Controller Error man:", error);
+        return res.status(500).json({ success: false, message: error.message });
+    }
+};
+
+
+export const FetchLinks = async (req, res) => {
+
+}
