74709434dd98e8e93fc8ab2ad49ef04b7f4487aa
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-05 21:57:26 +0800

    aa

--- client/src/pages/Dashboard/Redirect.jsx
+++ client/src/pages/Dashboard/Redirect.jsx
@@ -5,18 +5,22 @@ import { useParams } from "react-router-dom";
 
 export default function Redirect() {
     const { id } = useParams();
+    const en = import.meta.env.VITE_REDIRECT_BACKEND_URL
 
     useEffect(() => {
+
         const fetchLink = async () => {
             const res = await axios.get(
                 `${import.meta.env.VITE_REDIRECT_BACKEND_URL}/${id}`
             );
             //redirect
+
             window.location.replace(res.data.link?.links);
         };
 
         fetchLink();
+
     }, [id]);
 
-    return <div>Redirecting...</div>;
+    return <div>Redirecting... {en}</div>;
 }
