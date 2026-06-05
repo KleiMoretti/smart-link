bb53fc24359bbc4cd61fa4572c2bc5015306ee10
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-05 22:57:19 +0800

    a

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use("/api", authRoutes);
 
 app.get("/:code", Redirect);
 
-// 3. Gamitin ang PORT na ibibigay ng Render, o 5000 kung local
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
