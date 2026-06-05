8d477405e577f6a2ece4c33d64b945bae6171402
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-05 17:13:47 +0800

    a

--- server/db/supabaseClient.js
+++ server/db/supabaseClient.js
@@ -1,8 +1,9 @@
-import { createClient } from "@supabase/supabase-js"
+const { createClient } = require("@supabase/supabase-js");
 
 const supaBaseUrl = "https://esbrqvwhgkiwvuwdttks.supabase.co"
 const supaBaseKey = "sb_publishable_4mTba8ye7hn2Y0bO1JxXiw_WMmwc9Ul"
 
 const SupabaseConnect = createClient(supaBaseUrl, supaBaseKey)
 
-export default SupabaseConnect;
+
+module.exports = SupabaseConnect;
