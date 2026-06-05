2e63e0df15cac34b38851cedff3188cd8f1c70c7
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-05 17:13:42 +0800

    a

--- server/API/auth.routes.js
+++ server/API/auth.routes.js
@@ -1,13 +1,11 @@
-// Palitan ng require ang import
 const express = require("express");
-const { SaveLinks } = require("./link.controllers");
-const { GetLinks } = require("./link.controllers");
-const { Middleware } = require("./auth.middleware")
-
+const { SaveLinks, GetLinks, Redirect } = require("./link.controllers");
+const { Middleware } = require("./auth.middleware");
 
 const route = express.Router();
 
 route.post("/SaveLinks", Middleware, SaveLinks);
 route.get("/GetLinks", Middleware, GetLinks);
+route.get("/:code", Redirect); //
 
 module.exports = route;
