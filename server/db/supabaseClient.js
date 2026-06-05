98995cb883871fdc638e67ddc7eb51c681edd731
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-05 22:57:30 +0800

    a

--- server/db/supabaseClient.js
+++ server/db/supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 
-dotenv.config(); // Ensure env variables are loaded
+dotenv.config();
 
 const supaBaseUrl = process.env.SUPABASE_URL;
 const supaBaseKey = process.env.SUPABASE_KEY;
