fb5589574e7d6c84b180a1dbdcb7165096a91b7d
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-07 13:21:20 +0800

    a

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,58 +1,98 @@
 
 import "../../css/LandingPage.css"
 import { Link } from "react-router-dom"
+import Logo from "../../assets/logo.png"
+import "../../css/LandingPage.css"
+import LandingImage from "../../assets/ImageHome.jpg";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import { MoveY, MoveX } from "../../animation/Moves"
 
-export default function LandingPage() {
+export default function Test() {
+    const navigate = useNavigate();
+    const [ShowNav, setShowNav] = useState(true)
+    const MoveX1 = useRef();
+    const MoveX2 = useRef();
+    const MoveY1 = useRef();
+
+    useEffect(() => {
+        MoveX(MoveX1.current, -100, 0, 2);
+        MoveX(MoveX2.current, 100, 0, 2);
+        MoveY(MoveY1.current, 200, 0, 2);
+    }, []);
+
 
 
     return (
 
-        <section className="relative h-screen overflow-hidden p-10 min-w-[300px] min-h-[300px]">
-            {/* NAV */}
-            <nav className="flex justify-between items-center">
-                <div className="text-lg font-bold flex items-center leading-none w-fit whitespace-nowrap">
-                    FLUX LINK
-                </div>
-                <div className="hidden lg:flex gap-6 text-sm items-center">
-                    <div>
-                        HOME
+        <>
+            <nav className="w-full flex justify-center border boder-b-10 py-2 min-w-[200px]">
+                <div className="flex flex-nowrap lg:w-[60%] w-full justify-between items-center py-2 px-3">
+                    <div className="flex items-center">
+                        <img className="w-14" src={Logo} alt="" />
+                        <p className="m-0 whitespace-nowrap font-ubuntu">Flux Link</p>
                     </div>
-                    <div>
-                        ABOUT
+                    <div className="nav  flex gap-10 items-center">
+                        <p className="m-0 cursor-pointer">Home</p>
+                        <p className="m-0 cursor-pointer">About</p>
+                        <p className="m-0 cursor-pointer">Service</p>
+                        <p className="m-0 cursor-pointer">Contact</p>
                     </div>
-                    <div>
-                        SERVICE
-                    </div>
-                    <div className=" hover:bg-pink-500">
-                        CONTACT
-                    </div>
-                    <Link to={"/login"}>
-                        <div className="inline-flex items-center justify-center bg-black text-white px-4 py-2 rounded-md hover:bg-sky-700 transition-colors duration-300">
-                            <button>LOGIN</button>
+                    <div className="flex items-center justify-center">
+                        <div className="lg:flex hidden flex items-center justify-center bg-black text-white px-3 py-2 rounded-md cursor-pointer" onClick={() => navigate("/login")}>
+                            <p className="m-0">Login</p>
                         </div>
-                    </Link>
-                </div>
+                        <div className="relative">
+                            <i className="bi bi-list lg:hidden text-2xl flex cursor-pointer" onClick={() => setShowNav(prev => !prev)}></i>
+                            <div className={`absolute bg-white border border-gray-100 flex justify-center flex-col w-[100px] -ml-[80px] ${ShowNav ? "hidden" : "true"}`}>
+                                <p className="m-0 p-2 hover:bg-gray-300 cursor-pointer transition-color duration-300">Home</p>
+                                <p className="m-0 p-2 hover:bg-gray-300 cursor-pointer transition-color duration-300">About</p>
+                                <p className="m-0 p-2 hover:bg-gray-300 cursor-pointer transition-color duration-300">Service</p>
+                                <p className="m-0 p-2 hover:bg-gray-300 cursor-pointer transition-color duration-300">Contact</p>
+                                <p className="m-0 p-2 hover:bg-gray-300 cursor-pointer transition-color duration-300">Login</p>
+                            </div>
+                        </div>
 
-                <div className="flex lg:hidden items-center text-xl leading-none">
-                    <i className="bi bi-list"></i>
+                    </div>
                 </div>
             </nav>
 
-            {/* MAIN */}
-            <div className="main flex justify-center items-center gap-10 h-[80%] min-h-[90%] h-auto mt-[17px] lg:mt-0">
-                {/* ICON */}
-                <div className="flex justify-center items-center w-1/5">
-                    <i className="bi bi-link-45deg text-[25vw] leading-none"></i>
-                </div>
+            <div className="content-f1 flex w-full justify-center mt-15 min-w-[200px]">
+                <div className="text-wrapper flex justify-between w-[60%] p-3 gap-3">
+                    <div className="w-full flex  items-center text-[1vw]" ref={MoveX1}>
+                        <div>
+                            <div>
+                                <p className="main-text m-0 font-ubuntu font-medium whitespace-nowrap text-[2.5vw]">Smart Hyperlink</p>
+                            </div>
+                            <div>
+                                <p className=" main-text m-0 font-ubuntu font-medium whitespace-nowrap text-[2.5vw]">Time-based Redirection</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="subscript w-full flex flex-col gap-[1vw] text-[1vw]" ref={MoveX2}>
+                        <div className="subs1">
+                            <p className="m-0 font-medium  lg:whitespace-nowrap">One link, multiple destinations. Control where your</p>
+                            <p className="m-0 font-medium  lg:whitespace-nowrap">audience goes based on time and schedule.</p>
+                        </div>
 
-                {/* TEXT */}
-                <div className="text-2 w-1/4 text-[1.1vw] leading-relaxed">
-                    <p>
-                        <span className="title-2 font-bold">FLUXLINK</span> is a dynamic interactive system that reacts to motion-based triggers in real time. It uses collision detection to activate visual elements in a controlled sequence. The goal is to create smooth, intelligent transitions that feel responsive and natural.
-                    </p>
+                        <div className="subs2">
+                            <p className="m-0 whitespace-nowrap">Manage multiple destinations through a single hyperlink.</p>
+                            <p className="m-0 whitespace-nowrap"> Configure custom redirects for different days and  time periods, </p>
+                            <p className="m-0 whitespace-nowrap">allowing your links to adapt automatically to </p>
+                            <p className="m-0 whitespace-nowrap">changing schedules and content requirements.</p>
+                        </div>
+                    </div>
+                </div>
+            </div >
+
+            <div className="img w-full flex justify-center mt-15 p-2 min-w-[200px]">
+                <div className="img-parent w-[60%] justify-center">
+                    <img className="img-pic rounded-2xl" ref={MoveY1} src={LandingImage} alt="" />
                 </div>
             </div>
-        </section>
+
+        </>
+
 
     )
 
