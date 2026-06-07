7c07baf62872abfd7ede2b39427d816946b0afc5
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-07 23:37:05 +0800

    a

--- client/src/pages/Dashboard/Create.jsx
+++ client/src/pages/Dashboard/Create.jsx
@@ -5,6 +5,8 @@ import "../../css/LandingPage.css"
 
 export default function CreateLink() {
     const [input, setInput] = useState([{ title: "", link: "", day: "", time: "" }]);
+    const [title, setTitle] = useState("");
+    const [show, setShow] = useState(false);
 
     //handle submit
     const handleSubmit = async () => {
@@ -20,7 +22,8 @@ export default function CreateLink() {
 
             //fetching data
             const res = await axios.post(`${import.meta.env.VITE_API_SAVE_LINK}`, {
-                Links: input
+                Links: input,
+                Title: title
             }, {
                 headers: {
                     Authorization: `Bearer ${token}`
@@ -67,61 +70,79 @@ export default function CreateLink() {
     return (
         <>
             <div>
-                <div className="mt-10">
-                    {input.map((items, index) => (
-                        <div className="create-area justify-center flex flex-wrap gap-3 mt-6" key={index}>
-                            <div className="create-parent">
-                                <p>Title</p>
-                                <div className="bg-gray-200 rounded-lg p-2 h-10">
-                                    <input className="outline-none h-full bg-transparent" type="text" value={items.title} onChange={(e) => handleInput(e.target.value, index, "title")} />
-                                </div>
-                            </div>
-                            <div className="create-parent">
-                                <p>Link {index + 1}</p>
-                                <div className="bg-gray-200 rounded-lg p-2 h-10">
-                                    <input className="outline-none h-full bg-transparent" type="text" value={items.link} onChange={(e) => handleInput(e.target.value, index, "link")} />
-                                </div>
-                            </div>
-                            <div className="create-parent">
-                                <p>Day</p>
-                                <div className="bg-gray-200 rounded-lg p-2 h-10">
-                                    <select className="outline-none bg-transparent" value={items.day} onChange={(e) => handleInput(e.target.value, index, "day")}>
-                                        <option value="">Day</option>
-                                        <option value="Monday">Monday</option>
-                                        <option value="Tuesday">Tuesday</option>
-                                        <option value="Wednesday">Wednesday</option> {/* May typo yung Wenesday mo kanina */}
-                                        <option value="Thursday">Thursday</option>
-                                        <option value="Friday">Friday</option>
-                                        <option value="Saturday">Saturday</option>
-                                        <option value="Sunday">Sunday</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="create-parent">
-                                <p>Time</p>
-                                <div className="flex w-full gap-2 flex-wrap">
-                                    <div className="w-full bg-gray-200 rounded-lg p-2 h-10">
-                                        <input className="w-full h-full bg-transparent outline-none" type="time" value={items.time} onChange={(e) => handleInput(e.target.value, index, "time")} />
-                                    </div>
-                                    <div className={`w-full flex justify-center bg-red-500 py-2 px-3 text-white rounded-lg cursor-pointer ${index !== 0 || input.length > 1 ? "" : "hidden"}`} onClick={() => removeInput(index)}>
-                                        Delete
-                                    </div>
-                                </div>
+                <div className={`mt-10 ${show ? "hidden" : ""}`}>
+                    <div className="create-parent">
+                        <p>Schedule Name</p>
+                        <div className="w-full bg-gray-200 rounded-lg p-2 h-10">
+                            <input className="outline-none h-full bg-transparent" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+                        </div>
+                        <div className="flex gap-3 w-full justify-center mt-4">
+                            <div className={`w-full flex justify-center bg-black py-2 px-3 text-white rounded-lg cursor-pointer ${title.length > 2 ? "" : "hidden"}`} onClick={() => setShow(true)}>
+                                <button>Continue</button>
                             </div>
                         </div>
-                    ))}
-                </div>
-
-                <div className="flex justify-center mt-10 gap-10">
-                    <div className="bg-gray-200 py-2 px-3 rounded-lg cursor-pointer" onClick={add}>
-                        <button>Add</button>
-                    </div>
-
-                    <div className="bg-black py-2 px-3 text-white rounded-lg cursor-pointer" onClick={handleSubmit}>
-                        <button>Done</button>
                     </div>
                 </div>
-            </div>
+
+                {show && (
+                    <div>
+                        <div>
+                            {input.map((items, index) => (
+                                <div className="create-area justify-center flex flex-wrap gap-3 mt-6" key={index}>
+                                    <div className="create-parent">
+                                        <p>Title</p>
+                                        <div className="bg-gray-200 rounded-lg p-2 h-10">
+                                            <input className="outline-none h-full bg-transparent" type="text" value={items.title} onChange={(e) => handleInput(e.target.value, index, "title")} />
+                                        </div>
+                                    </div>
+                                    <div className="create-parent">
+                                        <p>Link {index + 1}</p>
+                                        <div className="bg-gray-200 rounded-lg p-2 h-10">
+                                            <input className="outline-none h-full bg-transparent" type="text" value={items.link} onChange={(e) => handleInput(e.target.value, index, "link")} />
+                                        </div>
+                                    </div>
+                                    <div className="create-parent">
+                                        <p>Day</p>
+                                        <div className="bg-gray-200 rounded-lg p-2 h-10">
+                                            <select className="outline-none bg-transparent" value={items.day} onChange={(e) => handleInput(e.target.value, index, "day")}>
+                                                <option value="">Day</option>
+                                                <option value="Monday">Monday</option>
+                                                <option value="Tuesday">Tuesday</option>
+                                                <option value="Wednesday">Wednesday</option>
+                                                <option value="Thursday">Thursday</option>
+                                                <option value="Friday">Friday</option>
+                                                <option value="Saturday">Saturday</option>
+                                                <option value="Sunday">Sunday</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="create-parent">
+                                        <p>Time</p>
+                                        <div className="delete-area flex gap-2 ">
+                                            <div className="w-full bg-gray-200 rounded-lg p-2 h-10">
+                                                <input className="w-full h-full bg-transparent outline-none" type="time" value={items.time} onChange={(e) => handleInput(e.target.value, index, "time")} />
+                                            </div>
+                                            <div className={`w-full flex justify-center bg-red-500 py-2 px-3 text-white rounded-lg cursor-pointer ${index !== 0 || input.length > 1 ? "" : "hidden"}`} onClick={() => removeInput(index)}>
+                                                Delete
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="flex justify-center mt-10 gap-10">
+                            <div className="bg-gray-200 py-2 px-3 rounded-lg cursor-pointer" onClick={add}>
+                                <button>Add</button>
+                            </div>
+
+                            <div className="bg-black py-2 px-3 text-white rounded-lg cursor-pointer" onClick={handleSubmit}>
+                                <button>Done</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div >
         </>
     );
 }
