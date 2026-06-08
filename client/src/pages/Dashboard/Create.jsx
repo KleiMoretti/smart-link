8fb17dfa0272292dcf7ea28c6d46848ab4f7cda8
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-08 10:33:21 +0800

    a

--- client/src/pages/Dashboard/Create.jsx
+++ client/src/pages/Dashboard/Create.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import { auth } from "../../firebase/firebase";
-import "../../css/LandingPage.css"
 
 export default function CreateLink() {
     const [input, setInput] = useState([{ title: "", link: "", day: "", time: "" }]);
