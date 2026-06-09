f2826cbf780a9a19b23f840c2225dd4ff7133691
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-09 20:26:31 +0800

    a

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -44,8 +44,6 @@ export default function Login() {
     return (
         <>
             <section className="px-3 py-3 h-screen relative overflow-hidden min-h-[300px] min-w-[200px]">
-
-
                 <div className="flex items-center h-10" ref={MoveX}>
                     <div className="flex gap-2 cursor-pointer" onClick={() => { navigate("/") }}>
                         <div><i className="bi bi-arrow-left"></i></div>
@@ -53,76 +51,52 @@ export default function Login() {
                     </div>
                 </div>
                 <div className="mt-10">
-
                     <div className="flex justify-center">
                         <div className="title">
                             <div className="flex gap-3 flex">
                                 <div className="flex gap-1">
-                                    <div>
-                                        <p>S</p>
-                                    </div>
-                                    <div>
-                                        <p>I</p>
-                                    </div>
-                                    <div>
-                                        <p>G</p>
-                                    </div>
-                                    <div>
-                                        <p>N</p>
-                                    </div>
+                                    <p>S</p>
+                                    <p>I</p>
+                                    <p>G</p>
+                                    <p>N</p>
                                 </div>
                                 <div className="flex gap-1">
-                                    <div>
-                                        <p>U</p>
-                                    </div>
-                                    <div>
-                                        <p>P</p>
-                                    </div>
+                                    <p>I</p>
+                                    <p>N</p>
                                 </div>
                                 <div className="flex gap-1">
-                                    <div>
-                                        <p>T</p>
-                                    </div>
-                                    <div>
-                                        <p>O</p>
-                                    </div>
+                                    <p>T</p>
+                                    <p>O</p>
                                 </div>
                                 <div className="flex gap-1 font-bold">
-                                    <div>
-                                        <p>F</p>
-                                    </div>
-                                    <div>
-                                        <p>L</p>
-                                    </div>
-                                    <div>
-                                        <p>U</p>
-                                    </div>
-                                    <div>
-                                        <p>X</p>
-                                    </div>
+                                    <p>F</p>
+                                    <p>L</p>
+                                    <p>U</p>
+                                    <p>X</p>
                                 </div>
                                 <div className="flex gap-1 font-bold">
-                                    <div>
-                                        <p>L</p>
-                                    </div>
-                                    <div>
-                                        <p>I</p>
-                                    </div>
-                                    <div>
-                                        <p>N</p>
-                                    </div>
-                                    <div>
-                                        <p>K</p>
-                                    </div>
+                                    <p>L</p>
+                                    <p>I</p>
+                                    <p>N</p>
+                                    <p>K</p>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                     <div className="flex justify-center">
-                        <div className="flex gap-2 justify-start items-center w-[300px] mt-3 p-3 cursor-pointer border border-gray-400 hover:bg-gray-100 rounded-lg" onClick={handleLogin}>
+                        <div className="flex gap-2 justify-start   items-center w-[300px] mt-3 p-3 cursor-pointer border border-gray-400 hover:bg-gray-200 rounded-lg" onClick={handleLogin}>
                             <div className="flex justify-start items-center">
-                                <i className="bi bi-google"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 48 48">
+                                    <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,
+                                    5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,
+                                    8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
+                                    <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,
+                                    7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
+                                    <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,
+                                    24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path>
+                                    <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,
+                                    0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+                                </svg>
                             </div>
                             <div className="flex justify-start items-center">
                                 <p className="m-0  whitespace-nowrap">Continue With Google Account</p>
