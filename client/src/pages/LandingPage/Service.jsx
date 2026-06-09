56d60ca6753e93493e65e595dfad47bb39792d12
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-09 20:26:40 +0800

    a

---
+++ client/src/pages/LandingPage/Service.jsx
@@ -0,0 +1,13 @@
+export default function Service() {
+    return (
+        <>
+            <section className="min-h-screen">
+                <div className="w-full justify-center">
+                    <div className="bg-red-200 w-[60%]">
+                        <p>a</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
