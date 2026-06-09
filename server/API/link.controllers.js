9989838c9129e94293b70e02bcb8be32cd63c60b
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-09 12:01:34 +0800

    a

--- server/API/link.controllers.js
+++ server/API/link.controllers.js
@@ -1,6 +1,7 @@
 import admin from "firebase-admin";
 
 import { SupabaseConnect } from "../db/supabaseClient.js";
+import e from "express";
 
 export const SaveLinks = async (req, res) => {
     try {
@@ -114,6 +115,7 @@ export const GetLinks = async (req, res) => {
             .from("Links")
             .select("title, links, day, time, code, schedule_name, id")
             .eq("uid", uid);
+            
 
         if (error) {
             return res.status(500).json({
@@ -195,5 +197,44 @@ export const Redirect = async (req, res) => {
 
 
 export const EditTable = async (req, res) => {
+    try {
+        if (!req.user || !req.user.uid) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
 
-}
+        const uid = req.user.uid;
+        const { editTable } = req.body;
+
+        if (!editTable) {
+            return res.status(400).json({ message: "No data" });
+        }
+
+        const { data, error } = await SupabaseConnect
+            .from("Links")
+            .update({
+                title: editTable.title,
+                links: editTable.link?.trim(),
+                day: editTable.day,
+                time: editTable.time,
+            })
+            .eq("uid", uid)
+            .eq("id", editTable.id);
+
+        if (error) {
+            return res.status(500).json({
+                success: false,
+                message: error.message,
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "saved",
+            link: data,
+        });
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Server error" });
+    }
+};
