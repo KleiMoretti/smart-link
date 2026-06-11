de669830d4ae86a94bd55020b38e4103ae02753c
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-11 15:11:23 +0800

    a

--- server/API/auth.routes.js
+++ server/API/auth.routes.js
@@ -1,6 +1,6 @@
 // API/auth.routes.js
 import express from "express";
-import { SaveLinks, GetLinks, EditTable } from "./link.controllers.js";
+import { SaveLinks, GetLinks, EditTable, DeleteLink } from "./link.controllers.js";
 import { Middleware } from "./auth.middleware.js";
 
 const route = express.Router();
@@ -8,5 +8,6 @@ const route = express.Router();
 route.post("/SaveLinks", Middleware, SaveLinks);
 route.get("/GetLinks", Middleware, GetLinks);
 route.post("/editable", Middleware, EditTable);
+route.post("/deletelink", Middleware, DeleteLink);
 
 export default route;
