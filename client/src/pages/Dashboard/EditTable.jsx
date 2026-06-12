c58d2f7e2a8c1c70dcfb98878a6180cdb8dfbac9
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-12 12:55:04 +0800

    a

--- client/src/pages/Dashboard/EditTable.jsx
+++ client/src/pages/Dashboard/EditTable.jsx
@@ -16,6 +16,8 @@ export default function Table({ profile, name, email }) {
     const [editId, setEditId] = useState(null);
     const [edit, setEdit] = useState({ title: "", link: "", day: "", time: "", id: "" });
     const [addRow, setAddRow] = useState([{ title: "", link: "", day: "", time: "", id: "" }]);
+    const [code, setCode] = useState("")
+
 
     const BackendRedirect = import.meta.env.VITE_REDIRECT_FRONTEND_URL || "";
 
@@ -33,7 +35,6 @@ export default function Table({ profile, name, email }) {
     };
 
 
-
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, async (user) => {
             if (!user) {
@@ -48,9 +49,13 @@ export default function Table({ profile, name, email }) {
                     headers: { Authorization: `Bearer ${token}` }
                 });
 
+
                 if (res.data?.success && res.data?.link) {
                     setLink(res.data?.link);
+
                     if (res.data.link.length > 0) setTitle(res.data.link[0].schedule_name);
+                    setCode(res.data.link[0].code)
+
                 }
             } catch (err) {
                 console.error(err);
@@ -67,13 +72,34 @@ export default function Table({ profile, name, email }) {
         const token = await user.getIdToken();
 
         const res = await axios.post(
-            "http://localhost:5000/api/editable",
+            `${import.meta.env.VITE_API_EDIT_TABLE}`,
             { editTable: edit },
             { headers: { Authorization: `Bearer ${token}` } }
         );
 
         if (res.data.success) {
             alert("success")
+
+        } else {
+            alert("failed man")
+        }
+    };
+
+    const handleSaveRow = async (id) => {
+        console.log(addRow);
+        const user = auth.currentUser;
+        const token = await user.getIdToken();
+
+        const res = await axios.post(
+            `${import.meta.env.VITE_API_SAVE_LINK_ROW}`,
+            { saveRow: addRow, saveCode: code, saveTitle: title },
+            { headers: { Authorization: `Bearer ${token}` } }
+        );
+
+        if (res.data?.success) {
+            alert("success")
+            setAddRow([{ title: "", link: "", day: "", time: "", id: "" }])
+
         } else {
             alert("failed man")
         }
@@ -102,8 +128,6 @@ export default function Table({ profile, name, email }) {
         } else {
             alert("Invalid men")
         }
-
-
     }
 
     const handleInput = (value, index, field) => {
@@ -121,6 +145,31 @@ export default function Table({ profile, name, email }) {
         setAddRow([...addRow, { title: "", link: "", day: "", time: "", id: "" }])
     }
 
+    const handleDeleteRow = (index) => {
+        if (!index) return
+        if (addRow.length === 1) {
+            alert("can't delete");
+            return;
+        }
+
+        setAddRow(addRow.filter((_, i) => i !== index))
+    }
+
+    const handleAutoSave = async (newName) => {
+        try {
+            const user = auth.currentUser;
+            const token = await user.getIdToken();
+
+            await axios.post(`${import.meta.env.VITE_API_AUTO_SAVE}`,
+                { saveTitle: title },
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+            console.log("Saved via backend!");
+        } catch (err) {
+            console.error("Save failed:", err);
+        }
+    };
+
     if (loading) return <p>Loading...</p>
 
     return (
@@ -128,9 +177,15 @@ export default function Table({ profile, name, email }) {
             <div className="flex items-center">
                 <div className="w-full">
                     <div className="title-link-main w-full font-medium lg:flex lg:flex-wrap justify-between items-center gap-2">
-                        <div className="flex items-center gap-1">
-                            <p className="link-title m-0 lg:text-4xl md:text-3xl text-2xl">{title}</p>
-                            <i className="m-0 bi bi-pencil-square cursor-pointer"></i>
+                        <div className="flex items-center gap-1 border-1 border-gray-300 w-fit">
+                            <input
+                                className=" m-width bg-slate-50 border border-slate-200 rounded-lg p-2 h-10 focus-within:border-sky-500 transition"
+                                type="text"
+                                placeholder="Enter schedule name"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                onBlur={() => handleAutoSave(title)}
+                            />
                         </div>
                         {links.length > 0 && links[0]?.code && (
                             <a href={`${BackendRedirect}${links[0].code}`} className="m-0 lg:text-lg text-gray-900 hover:underline break-all" target="_blank" rel="noreferrer">
@@ -163,18 +218,47 @@ export default function Table({ profile, name, email }) {
                         </div>
                     </div>
 
+                    <div className="mt-10">
+                        <p className="font-medium ">Add Schedule</p>
+                    </div>
+
                     <div className="flex justify-between">
                         <div className="flex gap-2 bg-gray-200 w-35 p-2 rounded-full hover:bg-gray-100 cursor-pointer items-center justify-center" onClick={handleAddRow}>
-                            <i class="bi bi-plus-circle"></i>
-                            <p className="m-0 whitespace-nowrap">Add Row</p>
+                            <i className="bi bi-plus-circle"></i>
+                            <p className="m-0 whitespace-nowrap">Add Schedule</p>
                         </div>
 
-                        <div className="flex gap-2 bg-gray-900 text-white w-35 p-2 rounded-full hover:bg-gray-700 cursor-pointer items-center justify-center transition">
+                        <div className="flex gap-2 bg-gray-900 text-white w-35 p-2 rounded-full hover:bg-gray-700 cursor-pointer items-center justify-center transition" onClick={handleSaveRow}>
                             <p className="m-0">Save</p>
                         </div>
-
-
                     </div>
+
+                    {addRow.map((item, index) => (
+                        <div key={index} className="create-area justify-between flex flex-wrap gap-4 mt-6 p-4 border border-slate-100 rounded-xl bg-white shadow-sm">
+                            <input className="m-width bg-slate-50 border border-slate-200 rounded-lg p-2 h-10 focus-within:border-sky-500 transition" type="text" placeholder="title"
+                                value={item.title} onChange={(e) => handleInput(e.target.value, index, "title")} />
+                            <input className="m-width bg-slate-50 border border-slate-200 rounded-lg p-2 h-10 focus-within:border-sky-500 transition" type="text" placeholder="title" type="text" placeholder="link"
+                                value={item.link} onChange={(e) => handleInput(e.target.value, index, "link")} />
+                            <select className="m-width bg-slate-50 border border-slate-200 rounded-lg p-2 h-10 focus-within:border-sky-500 transition" type="text" placeholder="title"
+                                value={item.day} onChange={(e) => handleInput(e.target.value, index, "day")}>
+                                <option value="">Day</option>
+                                <option value="Monday">Monday</option>
+                                <option value="Tuesday">Tuesday</option>
+                                <option value="Wednesday">Wednesday</option>
+                                <option value="Thursday">Thursday</option>
+                                <option value="Friday">Friday</option>
+                                <option value="Saturday">Saturday</option>
+                                <option value="Sunday">Sunday</option>
+                            </select>
+                            <input className="m-width bg-slate-50 border border-slate-200 rounded-lg p-2 h-10 focus-within:border-sky-500 transition" type="text" placeholder="title" type="time" placeholder="time"
+                                value={item.time} onChange={(e) => handleInput(e.target.value, index, "time")} />
+                            <div className={`flex justify-center items-center bg-red-50 hover:bg-red-100 text-red-600 py-2 px-3 rounded-lg cursor-pointer transition 
+                            ${index !== 0 || addRow.length > 1 ? "" : "hidden"}`}
+                                onClick={() => handleDeleteRow(index)}>
+                                Delete
+                            </div>
+                        </div>
+                    ))}
 
                     {(() => {
                         const filtered = showDay === "full week" ? links : links.filter(item => item.day === showDay);
@@ -192,21 +276,22 @@ export default function Table({ profile, name, email }) {
                                 <div key={index} className="mt-4">
                                     <p className="font-medium text-lg mb-2">{day}</p>
                                     {groups[day].map((item, index) => (
-                                        <div key={index} className="flex flex-wrap gap-4 items-center justify-between border border-gray-200 hover:border-gray-500 rounded-md hover:bg-gray-100 p-3 transition cursor-pointer mb-2">
-                                            <input className="m-0 flex-1 min-w-[100px] outline-none border border-gray-200 p-1" type="text" value={editId === item.id ? edit?.title || "" : item.title} onChange={(e) => setEdit(prev => ({ ...prev, title: e.target.value }))} onFocus={() => {
-                                                if (editId !== item.id) {
-                                                    setEditId(item.id);
-                                                    setEdit(item);
-                                                }
-                                            }} />
-                                            <input className="m-0 flex-1 min-w-[100px] outline-none border border-gray-200 p-1" type="text" placeholder={CutLength(item.links, 20)} value={editId === item.id ? edit?.link || item.links : item.links} onChange={(e) => setEdit(prev => ({ ...prev, link: e.target.value }))} onFocus={() => {
+                                        <div key={index} className="create-area justify-center flex flex-wrap gap-4 mt-6 p-4 border border-slate-100 rounded-xl bg-white shadow-sm">
+                                            <input className="m-width bg-slate-50 border border-slate-200 rounded-lg p-2 h-10 focus-within:border-sky-500 transition" value={editId === item.id ? edit?.title || "" : item.title} onChange={(e) => setEdit(prev => ({ ...prev, title: e.target.value }))} onFocus={() => {
                                                 if (editId !== item.id) {
                                                     setEditId(item.id);
                                                     setEdit(item);
                                                 }
                                             }} />
 
-                                            <select className="m-0 flex-1 min-w-[100px] outline-none border border-gray-200 p-1" type="text" placeholder={item.day} value={editId === item.id ? edit?.day || "" : item.day} onChange={(e) => setEdit(prev => ({ ...prev, day: e.target.value }))} onFocus={() => {
+                                            <input className="m-width bg-slate-50 border border-slate-200 rounded-lg p-2 h-10 focus-within:border-sky-500 transition" placeholder={CutLength(item.links, 20)} value={editId === item.id ? edit?.link || item.links : item.links} onChange={(e) => setEdit(prev => ({ ...prev, link: e.target.value }))} onFocus={() => {
+                                                if (editId !== item.id) {
+                                                    setEditId(item.id);
+                                                    setEdit(item);
+                                                }
+                                            }} />
+
+                                            <select className="m-width bg-slate-50 border border-slate-200 rounded-lg p-2 h-10 focus-within:border-sky-500 transition" placeholder={item.day} value={editId === item.id ? edit?.day || "" : item.day} onChange={(e) => setEdit(prev => ({ ...prev, day: e.target.value }))} onFocus={() => {
                                                 if (editId !== item.id) {
                                                     setEditId(item.id);
                                                     setEdit(item);
@@ -220,14 +305,15 @@ export default function Table({ profile, name, email }) {
                                                 <option value="Saturday">Saturday</option>
                                                 <option value="Sunday">Sunday</option>
                                             </select>
-                                            <input className="m-0 flex-1 min-w-[100px] outline-none border border-gray-200 p-1" type="text" placeholder={item.time} value={editId === item.id ? edit?.time || "" : item.time} onChange={(e) => setEdit(prev => ({ ...prev, time: e.target.value }))} onFocus={() => {
+
+                                            <input className="m-width bg-slate-50 border border-slate-200 rounded-lg p-2 h-10 focus-within:border-sky-500 transition" placeholder={item.time} value={editId === item.id ? edit?.time || "" : item.time} onChange={(e) => setEdit(prev => ({ ...prev, time: e.target.value }))} onFocus={() => {
                                                 if (editId !== item.id) {
                                                     setEditId(item.id);
                                                     setEdit(item);
                                                 }
                                             }} />
-                                            <div className="p-1 rounded-sm" onClick={() => handleSave(item.id)}><i className="text-blue-600 bi bi-save"></i></div>
-                                            <div className="p-1 rounded-sm" onClick={() => handleDelete(item.id)}><i className="text-red-400 bi bi-trash-fill"></i></div>
+                                            <div className="flex justify-center items-center bg-blue-50 hover:bg-blue-100  py-2 px-3 rounded-lg cursor-pointer transition text-blue-600" onClick={() => handleSave(item.id)}>Save</div>
+                                            <div className="flex justify-center items-center bg-red-50 hover:bg-red-100 text-red-600 py-2 px-3 rounded-lg cursor-pointer transition" onClick={() => handleDelete(item.id)}>Delete</div>
                                         </div>
                                     ))}
                                 </div>
@@ -236,29 +322,6 @@ export default function Table({ profile, name, email }) {
                             <div className="text-center text-gray-400 mt-10">No links found</div>
                         );
                     })()}
-
-                    <div className="mt-10">
-                        <p className="font-medium ">Add Column</p>
-                    </div>
-
-                    {addRow.map((item, index) => (
-                        <div key={index} className="flex justify-between bg-gray-100 p-2 mt-2">
-                            <input type="text" placeholder="title" value={item.title} onChange={(e) => handleInput(e.target.value, index, "title")} />
-                            <input type="text" placeholder="link" value={item.link} onChange={(e) => handleInput(e.target.value, index, "link")} />
-                            <select value={item.day} onChange={(e) => handleInput(e.target.value, index, "day")}>
-                                <option value="full week">Full Week</option>
-                                <option value="Monday">Monday</option>
-                                <option value="Tuesday">Tuesday</option>
-                                <option value="Wednesday">Wednesday</option>
-                                <option value="Thursday">Thursday</option>
-                                <option value="Friday">Friday</option>
-                                <option value="Saturday">Saturday</option>
-                                <option value="Sunday">Sunday</option>
-                            </select>
-                            <input type="time" placeholder="time" value={item.time} onChange={(e) => handleInput(e.target.value, index, "time")} />
-                        </div>
-                    ))}
-
                 </div>
             </div>
         </div>
