2f79692d359947a9d6d7c7eb30afe15259619822
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-14 12:02:27 +0800

    a

--- client/src/utils/CutLength.js
+++ client/src/utils/CutLength.js
@@ -1,4 +1,7 @@
-export const CutLength = (text, max) => {
-    const textLength = text.length > max ? text.slice(0, max) + "..." : text;
+export const CutLength = (text = "", max = 10) => {
+    const textLength = text.length > max
+        ? text.slice(0, max) + "..."
+        : text;
+
     return textLength;
-}
+};
