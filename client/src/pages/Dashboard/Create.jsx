0247858cc46b44ae5f36bae425daf31bdb8ee86e
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-14 19:59:44 +0800

    a

--- client/src/pages/Dashboard/Create.jsx
+++ client/src/pages/Dashboard/Create.jsx
@@ -13,14 +13,14 @@ export default function CreateLink() {
     return (
         <>
             <div className="flex flex-col justify-center items-center w-full ">
-                <div className="w-1/2 flex gap-10">
-                    <div className={`bg-gray-800 text-white p-3 w-1/2 rounded-lg cursor-pointer hover:scale-105 transition-transform delay-100 ease-in-out ${method === "" ? "" : "hidden"}`}
+                <div className="w-1/2 flex justify-center flex-wrap gap-10">
+                    <div className={`min-w-[200px] bg-gray-800 hover:bg-gray-700 text-white p-3 w-1/2 rounded-lg cursor-pointer hover:scale-105 transition-transform delay-100 ease-in-out ${method === "" ? "" : "hidden"}`}
                         onClick={() => handleMethod("MANUAL")}>
                         <p className="m-0 font-medium">Manual</p>
                         <p>Manually enter each schedule details one by one. More control, but slower.</p>
                     </div>
 
-                    <div className={`bg-gray-800 text-white p-3 w-1/2 rounded-lg cursor-pointer hover:scale-105 transition-transform delay-100 ease-in-out ${method === "" ? "" : "hidden"}`}
+                    <div className={`min-w-[200px] bg-gray-800 hover:bg-gray-700 text-white p-3 w-1/2 rounded-lg cursor-pointer hover:scale-105 transition-transform delay-100 ease-in-out ${method === "" ? "" : "hidden"}`}
                         onClick={() => handleMethod("AI")}>
                         <p className="m-0 font-medium">AI</p>
                         <p>Automatically extracts and saves your schedule from pasted text using AI. Fast and easy.</p>
