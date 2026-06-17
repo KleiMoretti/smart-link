844dc8eaa795a85d8e062ef27d9419332211a18e
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-17 20:09:23 +0800

    a

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from "react"
-import Logo from "../../assets/logo.png"
+import Logo from "../../assets/company-logo.png"
 import "../../css/LandingPage.css"
 import Table from "./Table"
 import { useNavigate } from "react-router-dom"
@@ -60,7 +60,7 @@ export default function Dashboard() {
                 <nav className="w-full flex justify-center border boder-b-10 py-2 min-w-[200px]">
                     <div className="flex flex-nowrap lg:w-[60%] w-full justify-between items-center py-2 px-3">
                         <div className="flex items-center cursor-pointer" onClick={() => navigate("/")}>
-                            <img className="w-14" src={Logo} alt="" />
+                            <img className="w-10" src={Logo} alt="" />
                             <p className="m-0 whitespace-nowrap font-ubuntu">Lyncks</p>
                         </div>
                         <div className="nav  flex flex-nowrap gap-10 items-center">
@@ -76,23 +76,44 @@ export default function Dashboard() {
                             <div className="relative">
                                 <i className="burger bi bi-list hidden text-2xl cursor-pointer" onClick={() => setShowNav(prev => !prev)}></i>
 
-                                <div className={` absolute bg-white border border-gray-100 flex justify-center flex-col w-fit -ml-[200px]  whitespace-nowrap rounded-sm   ${ShowNav ? "hidden" : ""}`}>
+                                <div className={` absolute bg-white  border border-gray-100 flex justify-center flex-col w-fit -ml-[200px]  whitespace-nowrap rounded-sm   ${ShowNav ? "hidden" : ""}`}>
 
-                                    <div className="p-2 w-full">
-                                        <div className={`flex items-center  m-0 cursor-pointer gap-2`}>
-                                            <img className="m-0 w-10 h-10 rounded-full" src={userDetails?.photoURL} alt="" onClick={ProfileSettings} />
-                                            <div className="gap-1 text-sm">
-                                                <p className="m-0 text-sm font-medium">{CutLength(userDetails?.displayName, 10)}</p>
+                                    <div className="p-2 w-full relative border border-gray-100">
+                                        {/* Trigger */}
+                                        <div
+                                            className="flex items-center cursor-pointer gap-2"
+                                            onClick={ProfileSettings}
+                                        >
+                                            <img
+                                                className="w-10 h-10 rounded-full"
+                                                src={userDetails?.photoURL}
+                                                alt=""
+                                            />
+
+                                            <div className="text-sm">
+                                                <p className="m-0 text-sm font-medium">
+                                                    {CutLength(userDetails?.displayName, 10)}
+                                                </p>
                                                 <p className="m-0 text-[10px]">{userDetails?.email}</p>
                                             </div>
-
                                         </div>
-                                        <div className={`absolute max-w-[300px] bg-white mt-3 -ml-50 border border-gray-100 p-2 shadow-sm rounded-lg ${profileSettings ? "" : "hidden"}`}>
-                                            <div className="border-gray-200 border-b p-2 text-gray-500">
-                                                <p className="m-0 text-sm font-medium">{userDetails?.displayName}</p>
-                                                <p className="m-0 text-[10px]">{userDetails?.email}</p>
+
+                                        {/* Dropdown */}
+                                        <div
+                                            className={`absolute right-0 mt-3 w-[260px] bg-white border border-gray-100 p-2 shadow-sm rounded-lg ${profileSettings ? "block" : "hidden"
+                                                }`}
+                                        >
+                                            <div className="border-b border-gray-200 p-2 text-gray-500">
+                                                <p className="text-sm font-medium">
+                                                    {userDetails?.displayName}
+                                                </p>
+                                                <p className="text-[10px]">{userDetails?.email}</p>
                                             </div>
-                                            <div className="w-full flex justify-center text-[15px] items-center bg-red-500 rounded-md text-white p-1 mt-4 cursor-pointer hover:bg-red-600" onClick={handleSignOUt}>
+
+                                            <div
+                                                className="w-full flex justify-center items-center bg-red-500 rounded-md text-white p-1 mt-4 cursor-pointer hover:bg-red-600"
+                                                onClick={handleSignOUt}
+                                            >
                                                 Log out
                                             </div>
                                         </div>
@@ -121,8 +142,6 @@ export default function Dashboard() {
                         </div>
                     </div>
                 </nav>
-
-
 
                 <div className="content-holder w-full flex justify-center mt-20 h-auto mb-10 min-w-[200px]">
                     {tab === "Table" && <Table />}
