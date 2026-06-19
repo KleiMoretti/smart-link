3cd7bf4c5c7d8b88ac45f904cc22d3102c50ebb2
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-19 10:52:23 +0800

    a

--- server/API/RateLimit.js
+++ server/API/RateLimit.js
@@ -1,13 +1,33 @@
-import rateLimit from "express-rate-limit"
 
-export const RateLimit = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
+//RateLimit.js
+import rateLimit, { ipKeyGenerator } from "express-rate-limit";
+import { RedisStore } from "rate-limit-redis";
+import { redisClient } from "../redisClient.js";
+
+export const RateLimit = ({ windowMs, max, message, prefix = "Request:" }) => {
+    return rateLimit({
+        windowMs,
+        max,
+        message,
+        standardHeaders: true,
+        legacyHeaders: false,
+
+        keyGenerator: (req) => { return `${prefix}${req.user?.uid || ipKeyGenerator(req)}`; },
+
+        store: new RedisStore({
+            sendCommand: (...args) => { return redisClient.sendCommand(args) },
+            prefix: "",
+        }),
+    });
+};
+
+
+export const RequestRL = RateLimit({
+    windowMs: 1000 * 60 * 5,
+    max: 10,
     message: {
         success: false,
-        message: "Too many Reqeust Wait 15 minutes"
+        message: "Too many requests, try again later"
     },
-    standardHeaders: true,
-    legacyHeaders: false,
-    ipv6Subnet: 56,
+    prefix: "Request:"
 })
