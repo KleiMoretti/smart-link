3291a76112006da5ad73530498db4903e3258c55
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-19 10:48:24 +0800

    a

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client' // Correct import
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './index.css'
-
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.jsx'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
