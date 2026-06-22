a5653b85400e23cc57640f0fb932c0ea53489faa
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-23 00:33:56 +0800

    a

--- client/src/TEST/LandingTest.jsx
+++ client/src/TEST/LandingTest.jsx
@@ -1,9 +1,15 @@
 import NavTest from "../TEST/NavTest"
+import HomeTest from "../TEST/Home"
 
 export default function LandingTest() {
     return (
         <>
+
             <NavTest />
+
+            <main>
+                <HomeTest />
+            </main>
         </>
     )
 }
