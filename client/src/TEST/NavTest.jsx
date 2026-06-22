e2d4e03a495faffca8d0383022464f75177e4f53
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-23 00:33:59 +0800

    a

--- client/src/TEST/NavTest.jsx
+++ client/src/TEST/NavTest.jsx
@@ -4,18 +4,18 @@ import "../TEST/General.css"
 export default function Navigation() {
     return (
         <>
-            <header className="fixed top-0 left-0 w-full z-50 border-b border-b-gray-200 py-3 px5">
-                <div className="bg-[rgb(var(--bg-white-72))] max-w-[1120px] m-auto flex justify-between items-center">
+            <header className="fixed top-0 left-0 w-full z-50 border-b border-b-gray-200 py-[12px] px-[16px]">
+                <div className="bg-[rgb(var(--bg-white-72))]  max-w-[1120px] m-auto flex justify-between items-center">
                     <div className="flex items-center gap-3">
                         <span className="w-[40px] overflow-hidden rounded-lg" >
                             <img src={LogoTest} alt="" />
                         </span>
 
                         <span>
-                            <p className="m-0 display-font font-bold tracking-tighter text-lg">Lyncks</p>
+                            <p className="m-0 font-['Space_Grotesk',sans-serif] font-bold tracking-tighter text-lg">Lyncks</p>
                         </span>
                     </div>
-                    <nav className="flex gap-3 font-semibold display-font">
+                    <nav className="flex gap-3 font-semibold text-[14px] font-['Space_Grotesk',sans-serif]">
 
                         <div className="py-2 px-3 rounded-full hover:bg-[rgb(0_7_255_/_16%)] transition-colors duration-300 ease-in-out">
                             <button>Home</button>
