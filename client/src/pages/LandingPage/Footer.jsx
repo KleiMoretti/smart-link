9f66020b62752fed1110966820031eec5d678724
kleimoretti <johnleonardgualdahara@gmail.com>
2026-06-24 17:17:39 +0800

    a

--- client/src/pages/LandingPage/Footer.jsx
+++ client/src/pages/LandingPage/Footer.jsx
@@ -4,9 +4,9 @@ export default function Footer() {
             <section className="border-t border-t-gray-200 py-[28px] px-0">
                 <div className="max-w-[1120px] my-0 mx-auto py-0 px-[32px] flex justify-between items-center">
                     <div>
-                        <p class="m-0 font-['Space_Grotesk',sans-serif] font-bold">Lyncks </p>
+                        <p className="m-0 font-['Space_Grotesk',sans-serif] font-bold  text-[clamp(20px,3vw,5px)]">Lyncks </p>
 
-                        <p class="m-0 font-['Space_Grotesk',sans-serif] ">Powered by Moretti</p>
+                        <p className="m-0 font-['Space_Grotesk',sans-serif] text-[clamp(12px,3vw,5px)]">Powered by Moretti</p>
                     </div>
                     <p className="m-0 font-['Inter',sans-serif] text-[12px]">© 2026 Lyncks. One link. Right on time.</p>
                 </div>
