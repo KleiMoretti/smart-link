18a83a337639b2514a569e84674d0e108f269ffa
kleimoretti <johnleonardgualdahara@gmail.com>
2026-07-02 14:05:07 +0800

    a

--- client/src/pages/Dashboard/AI.jsx
+++ client/src/pages/Dashboard/AI.jsx
@@ -21,29 +21,29 @@ export default function AI_TEST() {
     }
 
     return (
-        <section className="p-[140px] flex justify-center items-center bg-gray-100 min-h-screen">
-            <div className="w-1/2">
-                <p className="font-semibold ">Hello! I'm Moretti AI. What are we organizing today?</p>
-                <div className="bg-white px-4 py-3 rounded-full relative flex">
+        <section className="lg:py-[140px] py-40 px-2 lg:px-0  w-full lg:flex justify-center items-center bg-gray-100 min-h-screen ">
+            <div className="lg:w-1/2 w-full">
+                <p className="font-semibold text-[clamp(12px,2vw,18px)] whitespace-nowrap">Hello! I'm Moretti AI. What are we organizing today?</p>
+                <div className="bg-white px-4 py-3 rounded-full flex w-full">
                     <input type="text"
                         placeholder="Paste your schedule, and we'll organize the rest. example: Subject, Links, Time, Day"
-                        className="outline-none w-full"
+                        className="outline-none w-full placeholder:text-sm lg:placeholder:text-lg"
                         value={prompt}
                         onChange={(e) => setPrompt(e.target.value)}
                     />
                     <button onClick={() => SendPrompt()}><i className="bi text-3xl bi-arrow-up-circle-fill"></i></button>
                 </div>
-                <div className="flex justify-center gap-2 mt-2">
-                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3">
+                <div className="hidden lg:flex justify-center gap-2 mt-2">
+                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap">
                         Easy to use
                     </div>
-                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3">
+                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap">
                         Fast Setup
                     </div>
-                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3">
+                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap">
                         Well Oraganize
                     </div>
-                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3">
+                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap">
                         AI powered
                     </div>
                 </div>
