aeb6315d489c985c660b178d7e8926b28e1e60aa
kleimoretti <johnleonardgualdahara@gmail.com>
2026-07-02 19:52:03 +0800

    a

--- client/src/pages/Dashboard/AI.jsx
+++ client/src/pages/Dashboard/AI.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useState } from "react"
 import { getToken } from "../../utils/Token"
+import { useNavigate } from "react-router-dom";
 
 
 
 export default function AI_TEST() {
+    const navigate = useNavigate();
     const [prompt, setPrompt] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const features = ["Easy to use", "Fast Setup", "Well Organized", "AI powered"];
@@ -22,6 +24,10 @@ export default function AI_TEST() {
                     }
                 }
             )
+
+            setPrompt("")
+            navigate("/dashboard")
+
         } catch (err) {
             console.error(err);
         } finally {
@@ -31,10 +37,10 @@ export default function AI_TEST() {
 
     return (
 
-        <section className="lg:py-[140px] py-40 pb-5 px-2 lg:px-0 w-full flex lg:flex-row flex-col lg:justify-center lg:items-center overflow-hidden justify-end items-end bg-gray-100 min-h-screen ">
+        <section className="lg:py-[140px] py-40 pb-5 px-2 lg:px-0 w-full flex lg:flex-row flex-col lg:justify-center lg:items-center overflow-hidden justify-end items-end min-h-screen ">
             <div className="lg:w-1/2 w-full">
-                <p className="font-semibold text-center text-[clamp(12px,2vw,18px)] whitespace-nowrap font-['Space_Grotesk',sans-serif]">Hello! I'm Lyncks AI. What are we organizing today?</p>
-                <div className="bg-white px-4 py-3 rounded-full flex w-full">
+                <p className="font-semibold text-center text-[clamp(12px,2vw,18px)] whitespace-nowrap font-['Space_Grotesk',sans-serif]">Hello! I'm <span className="text-indigo-500">Lyncks</span> AI. What are we organizing today?</p>
+                <div className="bg-gray-100 px-4 py-3 rounded-full flex w-full border border-gray-100">
                     <input type="text"
                         placeholder="Paste your schedule, and we'll organize the rest. example: Subject, Links, Time, Day"
                         className="outline-none w-full placeholder:text-sm lg:placeholder:text-md placeholder:font-['Inter',sans-serif]"
