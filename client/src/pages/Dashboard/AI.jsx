edad2abcc97e14f4123bbff9e1375b87113d4b91
kleimoretti <johnleonardgualdahara@gmail.com>
2026-07-02 19:10:33 +0800

    a

--- client/src/pages/Dashboard/AI.jsx
+++ client/src/pages/Dashboard/AI.jsx
@@ -6,7 +6,7 @@ import { getToken } from "../../utils/Token"
 
 export default function AI_TEST() {
     const [prompt, setPrompt] = useState("");
-    const [isLoading, setIsLoading] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     const SendPrompt = async () => {
         if (!prompt.trim() || isLoading) return;
@@ -27,10 +27,11 @@ export default function AI_TEST() {
             setIsLoading(false);
         }
     }
-
+    //  w-full flex flex-col justify-end items-end bg-gray-100 min-h-screen 
 
     return (
-        <section className="lg:py-[140px] py-40 px-2 lg:px-0  w-full lg:flex justify-center items-center bg-gray-100 min-h-screen ">
+
+        <section className="lg:py-[140px] pb-5 px-2 lg:px-0 w-full flex lg:flex-row flex-col lg:justify-center lg:items-center  justify-end items-end bg-gray-100 min-h-screen ">
             <div className="lg:w-1/2 w-full">
                 <p className="font-semibold text-center text-[clamp(12px,2vw,18px)] whitespace-nowrap font-['Space_Grotesk',sans-serif]">Hello! I'm Lyncks AI. What are we organizing today?</p>
                 <div className="bg-white px-4 py-3 rounded-full flex w-full">
@@ -46,17 +47,17 @@ export default function AI_TEST() {
                         <i className="bi text-3xl bi-arrow-up-circle-fill"></i>
                     )}</button>
                 </div>
-                <div className="hidden text-sm lg:flex justify-start gap-2 mt-3 font-['Space_Grotesk',sans-serif]">
-                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap">
+                <div className=" flex justify-start gap-2 mt-3 font-['Space_Grotesk',sans-serif] flex-wrap">
+                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap text-[clamp(3px,3vw,15px)]">
                         Easy to use
                     </div>
-                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap">
+                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap text-[clamp(5px,3vw,15px)]">
                         Fast Setup
                     </div>
-                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap">
+                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap text-[clamp(5px,3vw,15px)]">
                         Well Oraganize
                     </div>
-                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap">
+                    <div className="bg-white border border-gray-100 rounded-full py-2 px-3  whitespace-nowrap text-[clamp(5px,3vw,15px)]">
                         AI powered
                     </div>
                 </div>
