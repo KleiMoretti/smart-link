414305a4968644fc9faf44f93da1776151c48038
kleimoretti <johnleonardgualdahara@gmail.com>
2026-07-02 13:17:35 +0800

    a

--- server/API/auth.routes.js
+++ server/API/auth.routes.js
@@ -2,10 +2,8 @@
 import express from "express";
 import {
     SaveLinks, GetLinks,
-    EditTable, DeleteLink,
-    SaveLinkRow, SaveTitle,
-    FeedBack, CheckFeedBack,
-    AskGemini, SaveEdit, SaveRow
+    AskGemini, SaveEdit,
+    SaveRow, DeleteEdit
 } from "./link.controllers.js";
 import { Middleware } from "./auth.middleware.js";
 import { RequestRL } from "./RateLimit.js"
@@ -13,16 +11,12 @@ import { RequestRL } from "./RateLimit.js"
 const route = express.Router();
 
 route.post("/SaveLinks", Middleware, SaveLinks);
-route.get("/GetLinks", Middleware, RequestRL, GetLinks);
-route.post("/edittable", Middleware, EditTable);
-route.post("/deletelink", Middleware, DeleteLink);
-route.post("/savelinkrow", Middleware, SaveLinkRow);
-route.post("/saveTitle", Middleware, SaveTitle);
-route.post("/sendFeedBack", Middleware, FeedBack);
-route.get("/checkfeedback", Middleware, CheckFeedBack);
+route.get("/GetLinks", Middleware, GetLinks);
 route.post("/ask-gemini", Middleware, AskGemini);
 route.post("/saveedit", Middleware, SaveEdit);
 route.post("/SaveRow", Middleware, SaveRow);
+route.post("/deleteedit", Middleware, DeleteEdit);
+
 
 
 export default route;
